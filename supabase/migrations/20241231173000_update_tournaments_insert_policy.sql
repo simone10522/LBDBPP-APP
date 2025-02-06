bc6fa1f8730e1d08e0f0d742747b1612d894f3a7
simone10522 <simone10522@gmail.com>
2025-02-06 23:07:07 +0100

    Delete supabase directory

--- supabase/migrations/20241231173000_update_tournaments_insert_policy.sql
+++
@@ -1,6 +0,0 @@
-DROP POLICY "Authenticated users can create tournaments" ON tournaments;
-
-    CREATE POLICY "Authenticated users can create tournaments"
-      ON tournaments FOR INSERT
-      TO authenticated
-      WITH CHECK (auth.uid() = created_by);
