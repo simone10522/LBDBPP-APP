44c8bf602c03ba518d59a21c186e60db4ebd0853
simone10522 <simone10522@gmail.com>
2025-02-06 23:07:16 +0100

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { Platform } from 'react-native';
-import Constants from 'expo-constants';
-
-const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
-const supabaseKey = Constants.expoConfig?.extra?.supabaseKey;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
